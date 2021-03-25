db34db9d83f2f98fa32cfc3c5ec080675f46ada6
Metron16 <metron16@gmail.com>
2021-03-25 10:13:02 +0100

    Feladatlista bemásolása a script-be

---
+++ script.js
@@ -0,0 +1,16 @@
+/*  
+Feladatok:
+1. Egy gombra kattintva kérd be a felhasználó nevét egy 
+felugró ablakban, majd írd ki egy p tag-be, 
+üdvözöllek + neve.
+
+2. Illessz be egy tetszőleges képet, és hozz 
+létre egy eseményt.
+
+3. Az oldal betöltését ellenőrizd, és üzenj a konzolon, 
+ha betöltött.
+
+4. Illessz be egy Lorem div-et és változtass 
+meg 3 tulajdonságot JS-ből.
+*/
+
